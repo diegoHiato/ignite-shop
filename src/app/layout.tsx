b92cd944f4e69727b5b2721cac6890b655763269
diegoHiato <diegofreitas_oliveira@proton.me>
2023-08-09 08:49:55 -0300

    Define global style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={`${roboto.className} bg-base-900 text-base-300`}>{children}</body>
     </html>
   )
 }
